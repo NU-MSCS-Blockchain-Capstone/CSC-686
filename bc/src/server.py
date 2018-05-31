9fa4cc16ba9000f4bda686d9dcd7d72b5cf85b2e
Rich Yonts <richard.yonts@am.sony.com>
2018-05-30 17:07:10 -0700

    Update server.py

--- bc/src/server.py
+++ bc/src/server.py
@@ -1,3 +1,5 @@
+from json import dumps
+
 from src.blockchain import Blockchain
 
 '''
@@ -7,6 +9,7 @@ handling the details of the endpoints specified by a node in the TrustNet.
 
 class Server:
     def __init__(self):
+        self.transactions = {}
         self.blockchain = Blockchain()
     
     def readAllBlocks(self):  # todo implement
@@ -21,14 +24,22 @@ class Server:
     def createNewBlock(self, request):  # todo implement
         pass
 
-    def readAllNodes(self):  # todo implement
-        pass
-
-    def updateWithNewNode(self, request):  # todo implement
-        pass
-
-    def createNewTransaction(self):  # todo implement
-        pass
+    def createNewTransaction(self, data):
+        requireds = [ 'id', 'request', 'sourceid', 'targetid']
+        for required in requireds:
+            if not data.get(required):
+                return 'invalid transaction; missing {}'.format(required), 400
+        if data['id'] in self.transactions:
+            return 'ok', 200
+        self.transactions[data['id']] = data
+        print('Server.createNewTransacton() added new transaction')
+        return 'ok', 201
+    
+    # ==============================================================================
+    # debugging endpoints for prototype use
+    # ==============================================================================
+    def getTransactions(self):
+        return "/transaction:getTransactions(" + dumps(self.transactions) + ")", 200
 
 '''
 hints:
